34c04ac9156094d8f32eda10ab01c3b03afc0b0d
davidhenley <henleydr@gmail.com>
2019-12-04 09:42:28 -0600

    Initial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
